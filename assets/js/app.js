6c9ed65ee0bc950b0f662f3d784c83c26c17b2bd
Vahe Poghosyan <vahe.poghosyan.js@gmail.com>
2019-06-21 20:07:45 +0400

    implementatin of cover

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+(function () {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})();
