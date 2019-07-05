52ae210d1e5597f290f2bdcb24ecf8f019e781b5
Vahe Poghosyan <vahe.poghosyan.js@gmail.com>
2019-07-05 19:25:55 +0400

    media implementation of home page

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,32 @@
             clickable: true,
         },
     });
+    var swiperRandomTrips = new Swiper('.random-trips-swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        autoplay: true
+        // when window width is <= 480px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+    });
     var swiper1 = new Swiper('.feedback-card-swiper-container', {
-        slidesPerView: 2,
-        spaceBetween: 87,
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // when window width is <= 480px
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
     });
 
 })();
